2400236cee1df13086c5c2d075f83a4c66da38bd
Gaston <gastonn.g.y@gmail.com>
2023-06-01 22:14:39 -0300

    agregue tarea.js

---
+++ app-tareas/Tarea.js
@@ -0,0 +1,10 @@
+const {v4 : uuidv4} = require("uuid")
+
+const Tarea = function(titulo){
+    this.id = uuidv4();
+    this.titulo = titulo;
+    this.estado = "pendiente"
+    this.fecha = new Date();
+}
+
+module.exports = Tarea
